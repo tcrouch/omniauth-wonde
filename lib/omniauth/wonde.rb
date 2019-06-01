8efff05a9b71aa98fdb7562c538429e47f13ff90
Tom Crouch <tom.crouch@gmail.com>
2019-06-01 14:15:41 +0100

    Generate gem skeleton

---
+++ lib/omniauth/wonde.rb
@@ -0,0 +1,8 @@
+require "omniauth/wonde/version"
+
+module Omniauth
+  module Wonde
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/omniauth/wonde_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Omniauth::Wonde do
+  it "has a version number" do
+    expect(Omniauth::Wonde::VERSION).not_to be nil
+  end
+end
