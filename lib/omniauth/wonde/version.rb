bea1a7a95fbbe53c8a07f41f7e06f4061e0b0fc5
Tom Crouch <tom.crouch@gmail.com>
2019-09-11 17:17:08 +0100

    Add rubocop

--- lib/omniauth/wonde/version.rb
+++ lib/omniauth/wonde/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module OmniAuth
   module Wonde
     VERSION = "0.1.0"
--- spec/omniauth/wonde_spec.rb
+++ spec/omniauth/wonde_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe OmniAuth::Wonde do
   it "has a version number" do
     expect(OmniAuth::Wonde::VERSION).not_to be nil
