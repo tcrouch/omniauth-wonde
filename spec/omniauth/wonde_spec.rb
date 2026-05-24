150e310d3d9b0f19e87e2fa9841c40c9a623cbbf
Tom Crouch <tom.crouch@gmail.com>
2026-05-24 15:55:17 +0100

    test: use be_nil matcher instead of be nil

--- spec/omniauth/wonde_spec.rb
+++ spec/omniauth/wonde_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe OmniAuth::Wonde do
   it "has a version number" do
-    expect(OmniAuth::Wonde::VERSION).not_to be nil
+    expect(OmniAuth::Wonde::VERSION).not_to be_nil
   end
 end
